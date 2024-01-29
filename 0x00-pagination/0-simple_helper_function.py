bcfe4d3616e675d89b59a5b54347cc31131d7198
Manuel <141195emmanuel@gmail.com>
2024-01-29 01:04:17 +0000

    task 0 debug

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,10 @@
+#!/usr/bin/env python3
+""" Pagination """
+
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple:
-    """
-    @page refers: the specific page required
-    @page_size: how much a specific page is
-    ex: for the following dataset [x for x in range(21)]
-        if a single page is 7 elements, the start and end indexes
-        of the first page would be 0, 7
-    """
-    return ((page_size * page) - page_size, (page_size * page))
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ returns tuple of size two containing a start index and an end index """
+    end_index = start_index + page_size - 1
+    return (start_index, end_index)
